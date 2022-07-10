8dc263c925d52087bbefe8f43f90d3e739d45204
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-07-10 19:26:02 +0300

    add student test

--- backend/tests/api/test_students.py
+++ backend/tests/api/test_students.py
@@ -1,3 +1,4 @@
+from app.api.students import StudentErrors
 from app.crud import students
 from fastapi.testclient import TestClient
 from sqlmodel import Session
@@ -17,14 +18,24 @@ def test_create_student_by_admin(
     assert student.tg_id == created_student["tg_id"]
 
 
-def test_create_student_by_admin2(
-    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+def test_create_student_by_user(
+    client: TestClient, user_token_headers: dict[str, str], db: Session
 ) -> None:
     student_in = get_student_in()
     data = student_in.dict()
-    r = client.post(f"/students/", headers=superuser_token_headers, json=data)
-    assert 200 <= r.status_code < 300
+    r = client.post(f"/students/", headers=user_token_headers, json=data)
+    assert r.status_code == 400
     created_student = r.json()
-    student = students.read_by_tg_id(db, tg_id=student_in.tg_id)
-    assert student
-    assert student.tg_id == created_student["tg_id"]
+    assert created_student["detail"]["err"] == str(StudentErrors.UserIsNotAdmin)
+
+
+def test_create_student_same(
+    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    student_in = get_student_in()
+    student = students.create(db, payload=student_in)
+    data = student_in.dict()
+    r = client.post(f"/students/", headers=superuser_token_headers, json=data)
+    assert r.status_code == 400
+    created_student = r.json()
+    assert created_student["detail"]["err"] == str(StudentErrors.StudentExists)
